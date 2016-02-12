85bc42c08a98c154c78c5b03e9ac9893053c80e3
Lyndsey Browning <lbrowning86@gmail.com>
2016-02-12 15:26:40 +0000

    Create british-date.js

---
+++ british-date.js
@@ -0,0 +1,21 @@
+// formats a date to British format
+
+const formatDate = function(d, separator) {
+  separator = separator || '/';
+  
+  const addZero = function (n) {
+  	return (n < 10) ? '0' + n : n;
+  };
+
+  d = new Date(d);
+  
+  let year = d.getFullYear(),
+  	  month = addZero(d.getMonth() + 1),
+  	  day = addZero(d.getDate());
+      
+  return day + separator + month + separator + year;
+};
+
+formatDate(new Date()); // 12/02/2016
+formatDate('2014-10-12 09:00 AM'); // 12/10/2014
+formatDate(new Date(), '-'); // 12-02-2016
